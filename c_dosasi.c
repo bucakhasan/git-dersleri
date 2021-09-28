423cfb8ba2501c1c057d2fa0b7fac860b7635b7d
hasan.bucak <hasan.bucak@asiselektronik.com.tr>
2021-09-28 15:59:31 +0300

    ekleme yapildi

--- c_dosasi.c
+++ c_dosasi.c
@@ -5,11 +5,19 @@ int topla(int a, int b)
 {
 	return a+b;
 }
+
+
+int cikar(int a, int b)
+{
+	return a-b;
+}
+
+
 int main()
 {
 
 printf("\n\n\n\n\n\nsonuc: %d\n\n\n\n\n",topla(5,6));
 
 
-
+printf("\n\n\n\n\n\nsonuc: %d\n\n\n\n\n", cikar(4,4));
 }
